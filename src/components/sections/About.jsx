94f86997e9556e324a9124ce32a3c6f155a33a12
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-08 12:59:50 -0400

    built about section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import plantOne from "../../images/Plant1.png";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <Container style={{ maxWidth: "500px" }}>
+      <Row className="d-flex align-items-center">
+        <Col xs={9} md={7} lg={9}>
+          <p>About</p>
+          <p style={{ color: "grey" }}>
+            Experienced developer with a strong customer service background.
+            Excels in team collaboration, problem-solving, and achieving company
+            objectives. Leveraging my customer service skills to resolve
+            challenges and create websites.
+          </p>
+        </Col>
+        <Col xs={3} md={3} lg={3}>
+          <img
+            src={plantOne}
+            alt="Plant"
+            style={{ width: "100%" }}
+            className="mx-auto d-block"
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default About
+export default About;
