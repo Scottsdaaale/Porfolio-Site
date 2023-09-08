00fecf50be1886fddd7a76b2dff75c18b0b8eee2
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-08 13:03:56 -0400

    added bio and image

--- src/components/sections/Intro.jsx
+++ src/components/sections/Intro.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import logoImage from "../../images/Me.png"; // Import the image
 
 function Intro() {
   return (
-    <div>Intro</div>
-  )
+    <Container style={{ maxWidth: "500px", marginTop: "",marginBottom: "50px" }}>
+      <Row className="align-items-center justify-content-center">
+        {/* Image */}
+        <Col xs={3} sm={2} md={2} lg={3}>
+          <img
+            src={logoImage}
+            alt="Logo"
+            className="img"
+            style={{ width: "100%", height: "100%", borderRadius: "50%" }}
+          />
+        </Col>
+        <Col xs={9} sm={10} md={9}  className="d-flex flex-column">
+          <h1 style={{ fontSize: "18px" }}>Hello, my name is Scotty...</h1>
+          <p style={{ fontSize: "15px", color: "grey", margin: "0" }}>
+            Developer, Musician, Cat and Plant Dad
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default Intro
+export default Intro;
