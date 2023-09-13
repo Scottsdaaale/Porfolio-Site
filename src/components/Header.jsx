38f14e16707e8505170861b96a7418d3b2ef72ce
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-13 11:52:15 -0400

    test header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
-import logoImage from '../images/pixil-frame-0 (1).png'; // Import your image here
+import logoImage from '../images/Pixel-logo.png'; // Import your image here
 
 function Header() {
 
   return (
     <Navbar>
       <Container>
-        {/* Replace text brand with image */}
         <Navbar.Brand href="#">
-          <img
+          {/* <img
             src={logoImage}
             alt="Cartoon Logo"
             height="70" 
-          />
+          /> */}
         </Navbar.Brand>
       </Container>
     </Navbar>
