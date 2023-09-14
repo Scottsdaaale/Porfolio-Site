315f752940232a9b6469d6ee411a6f248cdb755d
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-13 22:19:30 -0400

    styled and added content

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <Container style={{ maxWidth: "500px", marginBottom: "30px" }}>
+      <Row>
+        <Col xs={9} md={7} lg={9}>
+          <h2 style={{ marginBottom: "20px" }}>Contact</h2>
+          <Container>
+            <h3>
+              Github: 
+            </h3>
+          </Container>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default Contact
+export default Contact;
