86069269b47da96d6c7faed03a96471f9629ff3e
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-13 22:19:16 -0400

    styled and added content

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -4,18 +4,20 @@ import plantOne from "../../images/Plant1.png";
 
 function About() {
   return (
-    <Container style={{ maxWidth: "500px" }}>
+    <Container style={{ maxWidth: "500px", marginBottom: "30px" }}>
       <Row className="d-flex align-items-center">
         <Col xs={9} md={7} lg={9}>
-          <p>About</p>
-          <p style={{ color: "grey" }}>
-            Experienced developer with a strong customer service background.
-            Excels in team collaboration, problem-solving, and achieving company
-            objectives. Leveraging my customer service skills to resolve
-            challenges and create websites.
-          </p>
+          <h2 style={{marginBottom: "20px"}}>About</h2>
+          <Container>
+            <p>
+              Experienced developer with a strong customer service background.
+              Excels in team collaboration, problem-solving, and achieving
+              company objectives. Leveraging my customer service skills to
+              resolve challenges and create websites.
+            </p>
+          </Container>
         </Col>
-        <Col xs={3} md={3} lg={3}>
+        <Col xs={3} md={3} lg={2}>
           <img
             src={plantOne}
             alt="Plant"
