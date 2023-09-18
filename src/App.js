0ca4c00f5a08cb5852ee275e5a4a61f3bf9a33f2
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-18 17:50:38 -0400

    commenting out music route for now

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import Main from "./components/Main";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+import Music from "./components/Music"; // Import the Music component (you need to create this component)
 
 function App() {
   return (
-    <div className="App" style={{justifyContent: "center"}}>
+    <div className="App" style={{ justifyContent: "center" }}>
       <Router>
         <Header />
-        <Main className="main" />
+        <Routes>
+          <Route path="/" element={<Main className="main" />} />
+          {/* <Route path="/music" element={<Music />} />  */}
+        </Routes>
       </Router>
     </div>
   );
