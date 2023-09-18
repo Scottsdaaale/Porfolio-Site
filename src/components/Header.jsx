ffe6d3156b7ced83c12600e34e58af735e852c26
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-18 17:53:04 -0400

    added translustent images on scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,68 @@
-import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import computer from "../images/Computer2.png";
+import music from "../images/Speaker4.png";
+import externalLink from "../images/ExternalLink.png";
 
-function Header() {
+const Header = () => {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerWidth <= 768) {
+        if (window.scrollY > 0) {
+          setScrolling(true);
+        } else {
+          setScrolling(false);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <Navbar>
-      <Container>
-        <Navbar.Brand href="#">
-          {/* <img
-            src={logoImage}
-            alt="Cartoon Logo"
-            height="70" 
-          /> */}
+    <Navbar className={`sticky-top ${scrolling ? "scrolling" : ""}`}>
+      <Nav className="ms-auto">
+        {/* <Navbar.Brand as={Link} to="/" className={`hoverable ${scrolling ? "scrolling" : ""}`}>
+          <img
+            src={computer}
+            alt="Computer"
+            width="40"
+            height="40"
+            className="hoverable-image"
+            style={{ marginRight: "10px" }}
+          />
         </Navbar.Brand>
-      </Container>
+
+        <Navbar.Brand as={Link} to="/music" className={`hoverable ${scrolling ? "scrolling" : ""}`}>
+          <img
+            src={music}
+            alt="Music"
+            width="40"
+            height="40"
+            className="hoverable-image"
+            style={{ marginRight: "10px" }}
+          />
+        </Navbar.Brand> */}
+        <Navbar.Brand as={Link} to="https://linktr.ee/scottsdaaale" target="_blank" className={`hoverable ${scrolling ? "scrolling" : ""}`}>
+          <img
+            src={externalLink}
+            alt="External Link"
+            width="40"
+            height="40"
+            className="hoverable-image"
+            style={{ marginRight: "10px" }} 
+          />
+        </Navbar.Brand>
+      </Nav>
     </Navbar>
   );
-}
+};
 
 export default Header;
