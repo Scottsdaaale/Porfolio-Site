d95e683a8bc9ce4099738992492761ec7c8b4b31
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-18 17:52:22 -0400

    created files for music route

---
+++ src/components/Music/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Player from './Player';
+
+function Projects() {
+  return (
+    <div>
+      <h1>Die With Nothing</h1>
+      <h1>
+        [[TAPPPY]]
+      </h1>
+    </div>
+  )
+}
+
+export default Projects
