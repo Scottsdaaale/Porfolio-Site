4106335fedd5ec713ffddeecef333075ccbefa8b
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-19 14:16:32 -0400

    removed row

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ import About from "./Developer/About";
 import Experience from "./Developer/Experience";
 import Projects from "./Developer/Projects";
 import Contact from "./Developer/Contact";
-import { Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 function Main() {
   return (
