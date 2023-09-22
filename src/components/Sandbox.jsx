c10f8e1893e64467df0c790114529d3ae8e423d9
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-22 11:02:31 -0400

    created sandbox and added widget components

--- src/components/Sandbox.jsx
+++ src/components/Sandbox.jsx
@@ -1,18 +1,37 @@
 import React from "react";
-import "../Sandbox.css"
+import "../Sandbox.css";
 import Buttons from "./Sandbox/Buttons";
-import ProgressBar from "./Sandbox/CSS Animations/ProgressBar";
+import SandboxPopup from "./SandboxPopup";
+import ProgressBar from "./Sandbox/Widgets/ProgressBar";
+import ColorChange from "./Sandbox/Widgets/ColorChange";
+import Popup from "./Sandbox/Widgets/Popup";
+import Clock from "./Sandbox/Widgets/Clock";
+import Translator from "./Sandbox/Widgets/Translator/Translator";
 import { Container, Row, Col } from "react-bootstrap";
-
 
 function Sandbox() {
   return (
-    <Container>
+    <Container >
+      <SandboxPopup />
       <Row>
-        <Col>
-          <Buttons />
+        <Col className="mb-4">
           <ProgressBar />
         </Col>
+        <Col className="mb-4">
+          <ColorChange />
+        </Col>
+        <Col className="mb-4">
+          <Translator />
+        </Col>
+      </Row>
+      <Row>
+        <Col className="mb-4">
+          <Popup />
+        </Col>
+        <Col className="mb-4">
+          <Clock />
+        </Col>
+        <Col></Col>
       </Row>
     </Container>
   );
