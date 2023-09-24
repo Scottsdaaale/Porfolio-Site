c1fe3d8d52d239b4d26a51706ad3564c10c0b366
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-23 21:44:17 -0400

    added fadeinwrapper to elements

--- src/components/Developer/Intro.jsx
+++ src/components/Developer/Intro.jsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import logoImage from "../../images/Me3.png";
+import logoImage from "../../images/Me.gif";
+import FadeInWrapper from "../FadeInWrapper";
 
 function Intro() {
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    // Use a state variable to trigger the fade-in effect
+    setFadeIn(true);
+  }, []);
+
   return (
     <Container
       style={{ maxWidth: "500px", marginTop: "", marginBottom: "50px" }}
     >
       <Row className="align-items-center justify-content-center">
-        <Col xs={3} md={3} lg={3}>
-          <img
-            src={logoImage}
-            alt="Logo"
-            className="img"
-            style={{ width: "100%", height: "100%", borderRadius: "50%" }}
-          />
+        <Col xs={4} md={3} lg={3}>
+          <FadeInWrapper delay={0}>
+            <img
+              src={logoImage}
+              alt="Logo"
+              className={`img-fade-in ${fadeIn ? "fade-in" : ""}`} // Apply the "img-fade-in" class conditionally
+              style={{ width: "100%", height: "100%", borderRadius: "50%" }}
+            />
+          </FadeInWrapper>
         </Col>
-        <Col xs={9} md={9} className="d-flex flex-column">
-          <h1>Hello, my name is Scotty Peterson...</h1>
-          <p style={{ margin: "0" }}>Developer, Musician, Cat and Plant Dad</p>
+        <Col xs={7} md={9} className="d-flex flex-column">
+          <FadeInWrapper delay={1800}>
+            <h1
+              className={`h1-fade-in ${fadeIn ? "fade-in" : ""}`} // Apply the "h1-fade-in" class conditionally
+            >
+              Hello, my name is Scotty Peterson...
+            </h1>
+          </FadeInWrapper>
+          <FadeInWrapper delay={3000}>
+            <p
+              className={`p-fade-in ${fadeIn ? "fade-in" : ""}`} // Apply the "p-fade-in" class conditionally
+              style={{ margin: "0" }}
+            >
+              Developer, Musician, Cat and Plant Dad
+            </p>
+          </FadeInWrapper>
         </Col>
       </Row>
     </Container>
