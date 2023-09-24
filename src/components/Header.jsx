3e7e594d450c402189accb7f56b95f9fac550a9c
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-23 21:43:26 -0400

    added gif image for sandbox link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import computer from "../images/Computer2.png";
+import computer from "../images/Computer.gif";
 import music from "../images/Speaker4.png";
 import externalLink from "../images/ExternalLink.png";
+import tool from "../images/Tool2.gif";
 
 const Header = () => {
   const [scrolling, setScrolling] = useState(false);
@@ -29,7 +30,7 @@ const Header = () => {
   return (
     <Navbar className={`sticky-top ${scrolling ? "scrolling" : ""}`}>
       <Nav className="ms-auto">
-        {/* <Navbar.Brand as={Link} to="/" className={`hoverable ${scrolling ? "scrolling" : ""}`}>
+        <Navbar.Brand as={Link} to="/" className={`hoverable ${scrolling ? "scrolling" : ""}`}>
           <img
             src={computer}
             alt="Computer"
@@ -40,7 +41,7 @@ const Header = () => {
           />
         </Navbar.Brand>
 
-        <Navbar.Brand as={Link} to="/music" className={`hoverable ${scrolling ? "scrolling" : ""}`}>
+        {/* <Navbar.Brand as={Link} to="/music" className={`hoverable ${scrolling ? "scrolling" : ""}`}>
           <img
             src={music}
             alt="Music"
@@ -50,6 +51,16 @@ const Header = () => {
             style={{ marginRight: "10px" }}
           />
         </Navbar.Brand> */}
+        <Navbar.Brand as={Link} to="/sandbox" className={`hoverable ${scrolling ? "scrolling" : ""}`}>
+          <img
+            src={tool}
+            alt="Sandbox page"
+            width="40"
+            height="40"
+            className="hoverable-image"
+            style={{ marginRight: "10px" }}
+          />
+        </Navbar.Brand> 
         <Navbar.Brand as={Link} to="https://linktr.ee/scottsdaaale" target="_blank" className={`hoverable ${scrolling ? "scrolling" : ""}`}>
           <img
             src={externalLink}
