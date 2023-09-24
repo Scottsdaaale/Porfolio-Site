cd284af911dc32499c8c7d235d9f959d3e57d360
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-23 21:42:21 -0400

    removed unused styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./components/Main";
 import Header from "./components/Header";
 import Music from "./components/Music"; 
 import Sandbox from "./components/Sandbox"
+import FadeInWrapper from "./components/FadeInWrapper";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 
@@ -11,7 +12,7 @@ import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <div className="App" style={{ justifyContent: "center" }}>
+    <div className="App">
       <Router>
         <Header />
         <Routes>
