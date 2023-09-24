9557195840197ac3d31fcd8a2c60b8c03314d926
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-23 21:43:53 -0400

    added fadeinwrapper to components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,15 +5,24 @@ import Experience from "./Developer/Experience";
 import Projects from "./Developer/Projects";
 import Contact from "./Developer/Contact";
 import { Container } from "react-bootstrap";
+import FadeInWrapper from "./FadeInWrapper";
 
 function Main() {
   return (
     <Container>
         <Intro />
+      <FadeInWrapper delay={5000}>
         <About />
+      </FadeInWrapper>
+      <FadeInWrapper delay={5000}>
         <Experience />
+      </FadeInWrapper>
+      <FadeInWrapper delay={5000}>
         <Projects />
+      </FadeInWrapper>
+      <FadeInWrapper delay={5000}>
         <Contact />
+      </FadeInWrapper>
     </Container>
   );
 }
