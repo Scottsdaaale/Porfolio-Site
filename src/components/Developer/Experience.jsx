fadf0325de2786b246dd06a053a62041e80164cc
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-29 18:43:28 -0400

    added useimage toggle functionality

--- src/components/Developer/Experience.jsx
+++ src/components/Developer/Experience.jsx
@@ -1,54 +1,28 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { Tooltip } from 'react-tooltip'
-import saturo from "../../images/Saturo2.png";
-import liquid from "../../images/Liquid2.png";
-import flatiron from "../../images/Flatiron2.png";
+import { Tooltip } from "react-tooltip";
+import useImageToggle from "../useImageToggle";
+import saturoPng from "../../images/Saturo.png";
+import saturoGif from "../../images/Saturo.gif";
+import liquidPng from "../../images/Liquid2.png";
+import liquidGif from "../../images/Computer.gif";
+import flatironPng from "../../images/Flatiron2.png";
+import flatironGif from "../../images/Computer.gif";
 import certificatePdf from "../../images/FlatironCertificate.pdf";
 
-const jobData = [
-  {
-    company: "Saturo Sounds",
-    position: "WordPress Engineer",
-    duration: "Freelance | 07/2023 - 09/2023",
-    responsibilities: [
-      "Redesigned music label website, creating a modern and engaging design.",
-      "Utilized Elementor Pro and custom coding for a dynamic and responsive user interface.",
-      "Collaborated closely with the client's team to translate unique requirements into site features.",
-      "Simplified site management for clients through intuitive interfaces and thorough documentation, ensuring smooth updates even post-departure.",
-    ],
-    image: saturo,
-    link: "http://www.saturosounds.com",
-  },
-  {
-    company: "LiquidXYZ",
-    position: "Junior Fullstack Developer",
-    duration: "Freelance | 7/2022 - 10/2022",
-    responsibilities: [
-      "Designed effective and user-friendly page layouts and navigation.",
-      "Managed project priorities and timelines through clear communication.",
-      "Implemented front-end code using JavaScript and Object-Oriented Programming.",
-      "Utilized technologies like HTML, CSS, and XML for web development.",
-    ],
-    image: liquid,
-    link: "https://www.linkedin.com/company/liquid-xyz/",
-  },
-  {
-    company: "Flatiron School",
-    position: "Fullstack Engineer Apprentice",
-    duration: "02/2022 - 06/2022",
-    responsibilities: [
-      "Developed web applications using various programming languages.",
-      "Maintained up-to-date knowledge of technology trends and products.",
-      "Ensured accuracy and functionality of websites.",
-      "Validated code for security and cross-browser compatibility. Designed websites and large-scale web applications, documenting test plans and procedures.",
-    ],
-    image: flatiron,
-    link: certificatePdf,
-  },
-];
+function Job({
+  position,
+  company,
+  duration,
+  responsibilities,
+  png,
+  gif,
+  link,
+  tooltipContent,
+}) {
 
-function Job({ position, company, duration, responsibilities, image, link }) {
+  const jobImage = useImageToggle(png, gif);
+
   return (
     <Container>
       <Row>
@@ -56,16 +30,19 @@ function Job({ position, company, duration, responsibilities, image, link }) {
           <a
             href={link}
             target="_blank"
+            rel="noreferrer"
             data-tooltip-id="my-tooltip"
             data-tooltip-place="bottom"
-            data-tooltip-content={link}
+            data-tooltip-content={tooltipContent}
             data-tooltip-offset={30}
           >
             <img
-              src={image}
+              src={jobImage.currentImage}
               alt="Job"
               style={{ width: "100%" }}
               className="hoverable"
+              onMouseEnter={jobImage.handleMouseEnter} // Add this line
+              onMouseLeave={jobImage.handleMouseLeave}
             />
           </a>
         </Col>
@@ -87,12 +64,58 @@ function Job({ position, company, duration, responsibilities, image, link }) {
 }
 
 function Experience() {
+  const jobData = [
+    {
+      company: "Saturo Sounds",
+      position: "WordPress Engineer",
+      duration: "Freelance | 07/2023 - 09/2023",
+      responsibilities: [
+        "Redesigned music label website, creating a modern and engaging design.",
+        "Utilized Elementor Pro and custom coding for a dynamic and responsive user interface.",
+        "Collaborated closely with the client's team to translate unique requirements into site features.",
+        "Simplified site management for clients through intuitive interfaces and thorough documentation, ensuring smooth updates even post-departure.",
+      ],
+      png: saturoPng,
+      gif: saturoGif,
+      link: "http://www.saturosounds.com",
+      tooltipContent: "Saturosounds.com",
+    },
+    {
+      company: "LiquidXYZ",
+      position: "Junior Fullstack Developer",
+      duration: "Freelance | 7/2022 - 10/2022",
+      responsibilities: [
+        "Designed effective and user-friendly page layouts and navigation.",
+        "Managed project priorities and timelines through clear communication.",
+        "Implemented front-end code using JavaScript and Object-Oriented Programming.",
+        "Utilized technologies like HTML, CSS, and XML for web development.",
+      ],
+      png: liquidPng,
+      gif: liquidGif,
+      link: "https://www.linkedin.com/company/liquid-xyz/",
+      tooltipContent: "LiquidXYZ LinkedIn",
+    },
+    {
+      company: "Flatiron School",
+      position: "Fullstack Engineer Apprentice",
+      duration: "02/2022 - 06/2022",
+      responsibilities: [
+        "Developed web applications using various programming languages.",
+        "Maintained up-to-date knowledge of technology trends and products.",
+        "Ensured accuracy and functionality of websites.",
+        "Validated code for security and cross-browser compatibility. Designed websites and large-scale web applications, documenting test plans and procedures.",
+      ],
+      png: flatironPng,
+      gif: flatironGif,
+      link: certificatePdf,
+      tooltipContent: "Flatiron Certificate",
+    },
+  ];
   return (
     <Container style={{ maxWidth: "500px", marginBottom: "30px" }}>
       <h2 style={{ marginBottom: "30px" }}>Experience</h2>
 
-      {/* Initialize the tooltip */}
-      <Tooltip id="my-tooltip"  />
+      <Tooltip id="my-tooltip" />
 
       {jobData.map((job, index) => (
         <Job
@@ -101,8 +124,10 @@ function Experience() {
           position={job.position}
           duration={job.duration}
           responsibilities={job.responsibilities}
-          image={job.image}
+          png={job.png}
+          gif={job.gif}
           link={job.link}
+          tooltipContent={job.tooltipContent}
         />
       ))}
     </Container>
@@ -110,4 +135,3 @@ function Experience() {
 }
 
 export default Experience;
-
