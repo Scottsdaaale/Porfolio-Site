6e34a01f46dd379ecf7e65dfd3c6fa7eafb35c24
Scottsdaaale <scottsdaaale@gmail.com>
2023-09-29 18:43:00 -0400

    added useimage toggle functionality

--- src/components/Developer/About.jsx
+++ src/components/Developer/About.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import plantOne from "../../images/Plant1.png";
+import useImageToggle from "../useImageToggle"; // Import the custom hook
+import plantPng from "../../images/Plant1.png";
+import plantGif from "../../images/Computer.gif"; 
 
 function About() {
+  // Use the custom hook for the plant image
+  const plantImage = useImageToggle(plantPng, plantGif);
+
   return (
     <Container style={{ maxWidth: "500px", marginBottom: "30px" }}>
       <Row className="d-flex align-items-center">
         <Col xs={9} md={7} lg={9}>
-          <h2 style={{marginBottom: "20px"}}>About</h2>
+          <h2 style={{ marginBottom: "20px" }}>About</h2>
           <Container>
             <p>
               Experienced developer with a strong customer service background.
@@ -19,10 +24,12 @@ function About() {
         </Col>
         <Col xs={3} md={2} lg={2}>
           <img
-            src={plantOne}
+            src={plantImage.currentImage}
             alt="Plant"
             style={{ width: "100%" }}
             className="mx-auto d-block"
+            onMouseEnter={plantImage.handleMouseEnter}
+            onMouseLeave={plantImage.handleMouseLeave}
           />
         </Col>
       </Row>
