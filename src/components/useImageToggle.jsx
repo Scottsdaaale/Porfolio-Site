5841a3cde51bbf53b54cc053a04a0aa57df818f6
Scottsdaaale <scottsdaaale@gmail.com>
2023-12-15 21:12:24 -0500

    mobile uses gifs now

--- src/components/useImageToggle.jsx
+++ src/components/useImageToggle.jsx
@@ -1,20 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function useImageToggle(initialImage, alternateImage) {
   const [currentImage, setCurrentImage] = useState(initialImage);
 
-  const handleMouseEnter = () => {
-    setCurrentImage(alternateImage);
-  };
+  useEffect(() => {
+    const isMobileDevice = () => {
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+      // You can use additional logic to determine mobile devices more accurately
+      // For example, you might want to check for specific screen sizes using window.innerWidth
+    };
 
-  const handleMouseLeave = () => {
-    setCurrentImage(initialImage);
-  };
+    const handleResize = () => {
+      if (isMobileDevice()) {
+        setCurrentImage(alternateImage);
+      } else {
+        setCurrentImage(initialImage);
+      }
+    };
+
+    // Initial check on mount
+    handleResize();
+
+    // Listen for window resize events
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [initialImage, alternateImage]);
 
   return {
     currentImage,
-    handleMouseEnter,
-    handleMouseLeave,
   };
 }
 
