a07a8dc656e405f16fdddbebb704015ed957cf24
Scottsdaaale <scottsdaaale@gmail.com>
2023-12-18 19:57:31 -0500

    added redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers";
+import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import vistedSliceReducer from './visitedSlice';
+import blogReducer from './blogSlice';
 
-export default configureStore({
-  reducer: rootReducer,
-  // Other store configurations if needed
+const rootReducer = combineReducers({
+  blog: blogReducer,
+  visited: vistedSliceReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['blog'], 
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
+export default store;
