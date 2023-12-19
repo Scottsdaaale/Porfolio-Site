94b1858822ab850446d8be714cd303d47805b444
Scottsdaaale <scottsdaaale@gmail.com>
2023-12-18 22:39:37 -0500

    removed persist

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-tooltip/dist/react-tooltip.css";
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import store, { persistor } from './redux/store';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import App from "./App";
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
+    <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,14 @@
-import { combineReducers } from 'redux';
-import { configureStore } from '@reduxjs/toolkit';
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import vistedSliceReducer from './visitedSlice';
-import blogReducer from './blogSlice';
+import { combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import vistedSliceReducer from "./visitedSlice";
+
 
 const rootReducer = combineReducers({
-  blog: blogReducer,
   visited: vistedSliceReducer,
 });
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['blog'], 
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
 });
-
-export const persistor = persistStore(store);
 
 export default store;
