2c854a1a4c2eb496554e4c11c6049deebec573da
Scottsdaaale <scottsdaaale@gmail.com>
2023-12-18 22:40:20 -0500

    added html blog content writing functionality

--- src/components/Blog/BlogPost.jsx
+++ src/components/Blog/BlogPost.jsx
@@ -1,35 +1,35 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Container, Row, Col } from "react-bootstrap";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { Container } from "react-bootstrap";
 import { setVisitedRoot } from "../../redux/visitedSlice";
 import { useParams } from "react-router-dom";
+import useScrollAndDispatch from '../Tools/useScrollAndDispatch';
+import posts from "./posts";
 
 const BlogPost = () => {
   const dispatch = useDispatch();
-  const { id } = useParams(); // Get the 'id' parameter from the URL
-  const posts = useSelector((state) => state.blog.posts);
+  const { id } = useParams();
+  console.log(id);
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
-  useEffect(() => {
-    dispatch(setVisitedRoot(true));
-  }, [dispatch]);
+  useScrollAndDispatch(dispatch, setVisitedRoot(true));
 
   // Find the post with the matching ID
-  const post = posts.find((post) => post.id === parseInt(id));
+  const post = posts.find((post) => post.id === id);
 
   if (!post) {
     return <div>Post not found!</div>;
   }
 
+  // Function to render content using dangerouslySetInnerHTML
+  const renderHTMLContent = () => {
+    return { __html: post.content };
+  };
+
   return (
     <Container>
-      <img src={post.image} alt={post.title} width="40" height="40"/>
+      <img src={post.image} alt={post.title} width="70" height="70" />
       <h2>{post.title}</h2>
-
-      <p>{post.content}</p>
+      <div dangerouslySetInnerHTML={renderHTMLContent()} />
     </Container>
   );
 };
--- src/components/Blog/posts.js
+++ src/components/Blog/posts.js
@@ -1,21 +1,29 @@
 import computer from '../../images/Computer.gif'
 
 const posts = [
-    {
-      id: 1,
-      title: 'How I got Gay',
-      image: computer,
-      excerpt: 'I wish I was capping about this...',
-      content: 'if i was the gayest individual id be gay',
-    },
-    
-    {
-      id: 2,
-      title: 'Taking my first big...',
-      image: computer,
-      excerpt: "It was a sunday...",
-      content: 'if i was the biggest boyerntuse id be gay'
-    }
-  ];
+  {
+    id: "how-i-got-gay",
+    title: 'How I got Gay',
+    image: computer,
+    excerpt: 'I wish I was capping about this...',
+    content: `
+    <p>This is a paragraph about how I got gay.</p>
+      <pre>
+        This is some preformatted text:
+        - Point 1
+        - Point 2
+        - Point 3
+      </pre>
+      <p>Another paragraph in the post.</p>
+    `,
+  },
+  {
+    id: "taking-my-first-big",
+    title: 'Taking my first big...',
+    image: computer,
+    excerpt: "It was a sunday...",
+    content: 'if i was the biggest boyerntuse id be gay'
+  }
+];
 
   export default posts;
