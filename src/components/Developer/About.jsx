19d173dc47592d086fdee440a441b2e717a509f1
Scottsdaaale <scottsdaaale@gmail.com>
2023-12-21 11:36:58 -0500

    single quotes

--- src/components/Developer/About.jsx
+++ src/components/Developer/About.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import useImageToggle from "../useImageToggle";
-import plantPng from "../../images/Plant.png";
-import plantGif from "../../images/Plant.gif";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import useImageToggle from '../useImageToggle';
+import plantPng from '../../images/Plant.png';
+import plantGif from '../../images/Plant.gif';
 
 function About() {
   // Use the custom hook for the plant image
   const plantImage = useImageToggle(plantPng, plantGif);
 
   return (
-    <Container style={{ marginBottom: "30px" }}>
-      <Row className="d-flex align-items-center">
+    <Container style={{ marginBottom: '30px' }}>
+      <Row className='d-flex align-items-center'>
         <Col xs={9} md={7} lg={9}>
-          <h2 style={{ marginBottom: "20px" }}>About</h2>
+          <h2 style={{ marginBottom: '20px' }}>About</h2>
           <Container>
             <p>
               Creative web developer with 2 years of experience. Effective
@@ -27,9 +27,9 @@ function About() {
         <Col xs={3} md={2} lg={2}>
           <img
             src={plantImage.currentImage}
-            alt="Plant"
-            style={{ width: "100%" }}
-            className="mx-auto d-block"
+            alt='Plant'
+            style={{ width: '100%' }}
+            className='mx-auto d-block'
             onMouseEnter={plantImage.handleMouseEnter}
             onMouseLeave={plantImage.handleMouseLeave}
           />
