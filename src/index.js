ad3b215e69be2a539f5325525b56bfb3f0d04b3d
Scottsdaaale <scottsdaaale@gmail.com>
2024-01-04 16:14:08 -0500

    removed context

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-tooltip/dist/react-tooltip.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
 import store from './redux/store';
 import App from './App';
-const helmetContext = {};
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
-    <HelmetProvider context={helmetContext}>
+    <HelmetProvider>
       <App />
     </HelmetProvider>
   </Provider>,
